597c5778714820d282c4b804bc113ee7ceee5860
Andrew Shumway <andrew.shumway@lucidworks.com>
2019-01-12 10:58:04 -0700

    Make a QPL version of JS stage template

---
+++ javascript/QPL/minimalPipelineSample.js
@@ -0,0 +1,22 @@
+/* globals Java, logger*/
+(function () {
+    "use strict";
+
+    /**
+     *  Add more functions as needed _this.functionName = function(){} and call them via this.functionName();
+     *
+     * @param request :: QueryRequestAndResponse.Request object
+     * @param response :: QueryRequestAndResponse.Response object
+     * @param ctx :: Map<String, Object>
+     * @param collection :: String containing the collection name
+     * @param solrServer :: org.apache.solr.client.solrj.SolrClient instance pointing at `collection`
+     * @param solrServerFactory :: com.lucidworks.apollo.component.SolrClientFactory instance
+     * @returns {*}
+     */
+    return function(request, response, ctx, collection, solrServer, solrServerFactory) {
+        logger.debug('\n\n************JS pipeline script invoked.');
+
+        //TODO: add pipeline code
+    };
+
+})();
