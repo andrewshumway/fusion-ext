dabdd8bdbb0c12bed5b12e2ece54a07779fae6d8
Andrew Shumway <andrew.shumway@lucidworks.com>
2019-03-21 15:07:08 -0700

    Make LICENSE explicit in both getPut and javascript directories

--- javascript/QPL/minimalPipelineSample.js
+++ javascript/QPL/minimalPipelineSample.js
@@ -3,18 +3,20 @@
     "use strict";
 
     /**
-     *  Add more functions as needed _this.functionName = function(){} and call them via this.functionName();
+     * see https://doc.lucidworks.com/fusion/3.0/Search/Custom-JavaScript-Query-Stages.html for bad documentation
+     * They claim that params and ctx/_context are passed but only ctx is actually passed
      *
-     * @param request :: QueryRequestAndResponse.Request object
-     * @param response :: QueryRequestAndResponse.Response object
-     * @param ctx :: Map<String, Object>
-     * @param collection :: String containing the collection name
-     * @param solrServer :: org.apache.solr.client.solrj.SolrClient instance pointing at `collection`
-     * @param solrServerFactory :: com.lucidworks.apollo.component.SolrClientFactory instance
-     * @returns {*}
+     * @param request The com.lucidworks.apollo.pipeline.query.Request object
+     * @param response The com.lucidworks.apollo.pipeline.query.Response object (only after Solr query stage)
+     * @param ctx A reference to the container which holds a map over the pipeline properties. Used to update or modify this information for downstream pipeline stages.
+     * @param collection the name of the Fusion collection being queried
+     * @param solrServer  The Solr server instance that manages the pipeline’s default Fusion collection. All indexing and query requests are done by calls to methods on this object. See SolrClient for details.
+     * @param solrServerFactory The SolrCluster server used for lookups by collection name which returns a Solr server instance for a that collection, e.g.
      */
-    return function(request, response, ctx, collection, solrServer, solrServerFactory) {
+    return function(request,response , ctx, collection, solrServer, solrServerFactory) {
         logger.debug('\n\n************JS pipeline script invoked.');
+
+
 
         //TODO: add pipeline code
     };
--- javascript/QPL/sampleIPipeline.js
+++ javascript/QPL/sampleIPipeline.js
@@ -6,17 +6,16 @@
     //global variables for loading the util library.
     var util = undefined; //local name of the library to load.
 
-    /**
+     /**
      * see https://doc.lucidworks.com/fusion/3.0/Search/Custom-JavaScript-Query-Stages.html for bad documentation
      * They claim that params and ctx/_context are passed but only ctx is actually passed
      *
-     * @param request The Solr query information.
-     * @param response The Solr response information.
+     * @param request The com.lucidworks.apollo.pipeline.query.Request object
+     * @param response The com.lucidworks.apollo.pipeline.query.Response object (only after Solr query stage)
      * @param ctx A reference to the container which holds a map over the pipeline properties. Used to update or modify this information for downstream pipeline stages.
      * @param collection the name of the Fusion collection being queried
      * @param solrServer  The Solr server instance that manages the pipeline’s default Fusion collection. All indexing and query requests are done by calls to methods on this object. See SolrClient for details.
      * @param solrServerFactory The SolrCluster server used for lookups by collection name which returns a Solr server instance for a that collection, e.g.
-     * @returns {*}
      */
     function queryMain(request, response, ctx, collection, solrServer, solrServerFactory) {
         logger.debug('***** checking for Util library.');
